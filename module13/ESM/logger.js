a1a196f0cd1faeb8e36ff50495e636490cc9788b
Developer Nahida <ummenahida20@gmail.com>
2025-06-01 00:10:38 +0600

    create logger app

---
+++ module13/ESM/logger.js
@@ -0,0 +1,35 @@
+
+
+const path = require('path');
+const fs = require('fs')
+console.log(process.argv)
+
+const inputArguments = process.argv.slice(2);
+console.log(inputArguments)
+
+const text = inputArguments.join(' ');
+if (!text) {
+    console.error('Please provide a message to log.');
+    process.exit(1);
+}
+
+const logger = (message) => {
+    const timestamp = new Date().toISOString();
+    const fomattedMessage = `[${timestamp}] ${message} \n`;
+    console.log(fomattedMessage)
+
+    const filename = path.join(__dirname, 'log.txt');
+
+    fs.appendFile(filename, fomattedMessage, (err) => {
+        if (err) {
+            console.error('Error writing to log file:', err);
+            process.exit(1);
+        }
+
+        console.log('Log message saved successfully.');
+        process.exit(0);
+    })
+}
+
+logger(text)
+
