ee45ad1cae51d83624419b63de3b705e846d1d72
Developer Nahida <ummenahida20@gmail.com>
2025-06-04 03:07:23 +0600

    create server file in app folder

---
+++ module14/src/app/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import path from 'path';
+import fs from 'fs';
+
+const filePath = path.join(__dirname, '../../DB/todo.json');
+app.use(express.json())
+
+
+app.get('/', (req: Request , res: Response ) => {
+  res.send('this is my express app!')
+})
+
+app.get('/todos', (req: Request , res: Response ) => {
+
+  const data = fs.readFileSync(filePath, 'utf-8');
+  console.log(req.body)
+  res.send('this is my express app!')
+})
+
+
+
+export default app;
