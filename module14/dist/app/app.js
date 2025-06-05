bed42aecaa060b544dc75a6d0f6b85bec0e2c08c
Developer Nahida <ummenahida20@gmail.com>
2025-06-06 02:04:03 +0600

    some work in app.ts file

--- module14/dist/app/app.js
+++ module14/dist/app/app.js
@@ -7,14 +7,25 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
+const todosRoute_1 = require("./todos/todosRoute");
 const filePath = path_1.default.join(__dirname, '../../DB/todo.json');
+// all middleware 
 app.use(express_1.default.json());
+app.use('/todos', todosRoute_1.todosRouter);
 app.get('/', (req, res) => {
-    res.send('this is my express app!');
+    res.send('this is my todos project');
 });
-app.get('/todos', (req, res) => {
+app.post('/createTodo', (req, res) => {
+    console.log('created todo route');
+    res.send('created todo');
+});
+app.get('/users', (req, res) => {
+    console.log('this is user route');
+    res.send('this is users');
+});
+app.get('/todo', (req, res) => {
     const data = fs_1.default.readFileSync(filePath, 'utf-8');
     console.log(req.body);
-    res.send('this is my express app!');
+    res.send(JSON.parse(data));
 });
 exports.default = app;
