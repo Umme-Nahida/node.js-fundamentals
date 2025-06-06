175b9948c9816cc2f00d588273fb56932c3c10cb
Developer Nahida <ummenahida20@gmail.com>
2025-06-07 02:06:59 +0600

    create simple DB file

--- module14/src/app/app.ts
+++ module14/src/app/app.ts
@@ -2,28 +2,33 @@ import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 import path from 'path';
 import fs from 'fs';
-
+import { todosRouter } from './todos/todosRoute';
 const filePath = path.join(__dirname, '../../DB/todo.json');
+
+// all middleware 
 app.use(express.json())
-
-
-const todosRouter = express.Router();
-const userRouter = express.Router();
-
 app.use('/todos', todosRouter)
 
 
 app.get('/', (req: Request , res: Response ) => {
-  res.send('this is my express app!')
+  res.send('this is my todos project')
 })
 
-app.get('/todos', (req: Request , res: Response ) => {
+app.post('/createTodo', (req : Request, res: Response)=>{
+  console.log('created todo route')
+  res.send('created todo')
+})
 
+app.get('/users', (req : Request, res: Response)=>{
+  console.log('this is user route')
+  res.send('this is users')
+})
+
+app.get('/todo', (req: Request , res: Response ) => {
   const data = fs.readFileSync(filePath, 'utf-8');
   console.log(req.body)
-  res.send('this is my express app!')
+  res.send( JSON.parse(data))
 })
-
 
 
 export default app;
