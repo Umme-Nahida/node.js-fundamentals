254aaae625b2f0500e195675ff9607a8df22a74e
Developer Nahida <ummenahida20@gmail.com>
2025-06-08 02:08:47 +0600

    connect mongodb and Uri

--- module14/src/app/server.ts
+++ module14/src/app/server.ts
@@ -1,12 +1,29 @@
- import app from "./app";
- const port = 5000;
+import app from "./app";
+const port = 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
- let server; 
+let server;
 
- const startServer = async ( )=>{
-     server = app.listen(port, () => {
-     console.log(`Example app listening on port ${port}`)
- })
- }
 
- startServer()
+const uri = `mongodb+srv://todoList:UQlmNVtFBzo8H6jW@cluster0.ytj0kf8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const startServer = async () => {
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    server = app.listen(port, () => {
+        console.log(`Example app listening on ports ${port}`)
+    })
+}
+
+startServer()
