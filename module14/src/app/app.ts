da05c3aceed021595b5447d49e06832fec8e8527
Developer Nahida <ummenahida20@gmail.com>
2025-06-10 00:21:20 +0600

    get single todo

--- module14/src/app/app.ts
+++ module14/src/app/app.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import fs from 'fs';
 import { todosRouter } from './todos/todosRoute';
 import { client } from '../mongodb';
+import { ObjectId } from 'mongodb';
 const filePath = path.join(__dirname, '../../DB/todo.json');
 
 // all middleware 
@@ -11,32 +12,45 @@ app.use(express.json())
 app.use('/todos', todosRouter)
 
 
-app.get('/', (req: Request , res: Response ) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('this is my todos project')
 })
 
-todosRouter.post('/createTodo', async(req : Request, res: Response)=>{
-   const testTodo = req.body;
-    const db = client.db("todoList");
-    const collection = db.collection("todosCollection")
-    await collection.insertOne(testTodo) 
-    .then(()=>{
-        console.log("inserted todo ")
+todosRouter.post('/createTodo', async (req: Request, res: Response) => {
+  const testTodo = req.body;
+  console.log(testTodo)
+  const db = client.db("todoList");
+  const collection = db.collection("todosCollection")
+  await collection.insertOne(testTodo)
+    .then(() => {
+      console.log("inserted todo ")
     })
 
-    const todos = await collection.find({}).toArray()
+  const todos = await collection.find({}).toArray()
   res.json(todos)
 })
 
-app.get('/users', (req : Request, res: Response)=>{
+// get single todo by id 
+todosRouter.get('/:id', async (req: Request, res: Response) => {
+  const db = client.db("todoList");
+  const collection = db.collection("todosCollection")
+  const id = req.params.id;
+  console.log(id)
+  const query = { _id: new ObjectId(id) };
+  const todo = await collection.findOne(query);
+  res.json(todo);
+
+})
+
+app.get('/users', (req: Request, res: Response) => {
   console.log('this is user route')
   res.send('this is users')
 })
 
-app.get('/todo', (req: Request , res: Response ) => {
+app.get('/todo', (req: Request, res: Response) => {
   const data = fs.readFileSync(filePath, 'utf-8');
   console.log(req.body)
-  res.send( JSON.parse(data))
+  res.send(JSON.parse(data))
 })
 
 
